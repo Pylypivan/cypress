dcbdc6a16336999913fb99f61804b93f643a75eb
pylypIvan <pilipivan1997@gmail.com>
2021-02-25 20:00:59 +0200

    final step

--- cypress/integration/examples/testTask.js
+++ cypress/integration/examples/testTask.js
@@ -1,5 +1,9 @@
 ///  <reference types = "cypress" />
 
+const getValue = (str) => {
+    return Cypress.env(str); 
+};
+
 const PAGE_NAME = 'QA Portal Login';
 const NO_ACC_WITH_USERNAME = 'No account found with that username.';
 const ENTER_USERNAME = 'Please enter username.';
@@ -9,18 +13,20 @@ const ENTER_PASSWORD = 'Please enter your password.';
 describe('Check all functionality Login form', () => {
 
     beforeEach(() => {
-
         cy.visit('https://web.pecodesoftware.com/qa-portal/registerlogin/registerlogin.php');
-
         cy.url().should('include', 'web.pecodesoftware');
-
       });
     
     it('Verify title', () => {
-
         cy.title().should('eq', 'Login');
     }); 
 
+    it('Check Logo image and main form for availability ', () => {
+        
+        cy.get('#logomini').should('not.be.hidden');
+        cy.get('.wrapper').should('not.be.hidden');
+    });    
+    
     it('Verify login page name', () => {
         
         cy.get('#all > div > center > h1').should('have.text', PAGE_NAME);
@@ -37,97 +43,55 @@ describe('Check all functionality Login form', () => {
     it('Click button Login when password and login are empty', () => {
         
         cy.get("[type = 'text']").should('be.visible');
-
         cy.get("[type = 'password']").should('be.visible');
-
+        cy.get('.btn-success').should('be.visible');
         cy.get('.btn-success').click();
-
         cy.get('#all > div > form > div:nth-child(1) > span')
-        
         .should('have.text', ENTER_USERNAME);
-
         cy.get('#all > div > form > div:nth-child(2) > span')
-        
         .should( 'have.text', ENTER_PASSWORD);
-       
-        cy.wait(3000);
     });
 
-    it('Type valid data in fields input username and password toLowerCase', () => {
+    it('Type valid data in the fields input username and password toLowerCase', () => {
         
-        cy.get("[type = 'text']").type('pylypIvan');
-
-        cy.get("[type = 'password']").type('qwerty12345');
-
-        cy.wait(3000);
-
+        cy.get("[type = 'text']").type(getValue('validName'));
+        cy.get("[type = 'password']").type(getValue('validPass'));
         cy.get('.btn-success').click();
-
         cy.get('#all > div > form > div.form-group.has-error > span')
-
-        .should('have.text', NO_ACC_WITH_USERNAME);
-
-    });
-
-    it('Type valid data in fields input username and password toUpperCase', () => {
-        
-        cy.get("[type = 'text']").clear().type('PYLYPIVAN');
-
-        cy.get("[type = 'password']").clear().type('QWERTY12345');
-
-        cy.wait(3000);
-
-        cy.get('.btn-success').click(); 
-
-        cy.get('#all > div > form > div.form-group.has-error > span')
-
         .should('have.text', NO_ACC_WITH_USERNAME);
     });
 
     it('Type invalid data in fields input username and password', () => {
         
-        cy.get("[type = 'text']").clear().type('**-/~~+-.,rand@mName');
-
-        cy.get("[type = 'password']").clear().type('1*2#3~4//5aBc(.');
-
-        cy.wait(3000);
-
+        cy.get("[type = 'text']").clear().type(getValue('invalidName'));
+        cy.get("[type = 'password']").clear().type(getValue('invalidPass'));
         cy.get('.btn-success').click();
-
         cy.get('#all > div > form > div.form-group.has-error > span')
-
         .should('have.text', NO_ACC_WITH_USERNAME);
-
-         cy.get("[type = 'text']").clear();
-         cy.get("[type = 'password']").clear();
+         
     });
 
     it('Type data in the field input username without password ', () => {
         
-        cy.get("[type = 'text']").type('randomUser');
-
+        cy.get("[type = 'text']").type(getValue('validName'));
         cy.get('.btn-success').click();
-
-        cy.wait(2000);
-
         cy.get('#all > div > form > div:nth-child(2) > span')
-        
         .should( 'have.text', ENTER_PASSWORD);
     });
 
     it('Type data in the field input password without username ', () => {
-        
-        cy.get("[type = 'password']").type('12345password');
 
+        cy.get("[type = 'password']").type(getValue('validPass'));
         cy.get('.btn-success').click();
-
-        cy.wait(2000);
-
         cy.get('#all > div > form > div:nth-child(1) > span')
-        
         .should( 'have.text', ENTER_USERNAME);
     });
+
+    it('Successful Login', () => {
+
+        cy.get("[type = 'password']").type(getValue('validPass'));
+        cy.get('.btn-success').click();
+        cy.title().should('eq', 'Home Page');
+    });
 });
-
-
 
